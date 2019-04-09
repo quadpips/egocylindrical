516e2ea85d7104cd17342826fe8878f4b751e7fd
Justin Smith <jssmith@gatech.edu>
2019-04-09 19:08:14 -0400

    Minor cleanup

--- include/egocylindrical/egocylinder_msg_details.h
+++ include/egocylindrical/egocylinder_msg_details.h
@@ -9,19 +9,13 @@ namespace egocylindrical
 {
     namespace utils
     {
-        typedef boost::alignment::aligned_allocator<void, 32> AlignedAllocator;
         //ROS_DECLARE_MESSAGE_WITH_ALLOCATOR(EgoCylinderPoints, ECMsg, std::allocator<void> );
         
-        //template <typename T>
         //using Allocator = std::allocator<void>;
         using Allocator = boost::alignment::aligned_allocator<void, __BIGGEST_ALIGNMENT__>;
         
-        //typedef ::egocylindrical::EgoCylinderPoints_<Eigen::aligned_allocator<void, 32> > AlignedEgoCylinderPoints;
         typedef ::egocylindrical::EgoCylinderPoints_<utils::Allocator> ECMsg;
-        
-        // NOTE: I'm not sure that using this typedef renamed version was such a good idea after all...
-        //typedef AlignedEgoCylinderPoints ECMsg;
-        //typedef EgoCylinderPoints ECMsg;
+
         typedef boost::shared_ptr<ECMsg> ECMsgPtr;
         typedef boost::shared_ptr<ECMsg const> ECMsgConstPtr;
     }
