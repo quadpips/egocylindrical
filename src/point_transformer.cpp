177c216a5d94f13855fe533438d7456db42196fb
Max Asselmeier <mass@gatech.edu>
2025-01-14 16:24:33 -0500

    added normals back to transform_impl

--- src/point_transformer.cpp
+++ src/point_transformer.cpp
@@ -35,9 +35,9 @@ namespace egocylindrical
             const float* z = (const float*)points.getZ();
 
             // Normals
-            // const float* norm_x = (const float*)__builtin_assume_aligned(points.getNormalX(), __BIGGEST_ALIGNMENT__);
-            // const float* norm_y = (const float*)points.getNormalY();
-            // const float* norm_z = (const float*)points.getNormalZ();
+            const float* norm_x = (const float*)__builtin_assume_aligned(points.getNormalX(), __BIGGEST_ALIGNMENT__);
+            const float* norm_y = (const float*)points.getNormalY();
+            const float* norm_z = (const float*)points.getNormalZ();
 
             // Labels
             const uint8_t* labels = (const uint8_t*)points.getLabels();
@@ -48,9 +48,9 @@ namespace egocylindrical
             float* z_n = (float*)transformed_points.getZ();
             
             // New normals
-            // float* norm_x_n = (float*)__builtin_assume_aligned(transformed_points.getNormalX(), __BIGGEST_ALIGNMENT__);
-            // float* norm_y_n = (float*)transformed_points.getNormalY();
-            // float* norm_z_n = (float*)transformed_points.getNormalZ();
+            float* norm_x_n = (float*)__builtin_assume_aligned(transformed_points.getNormalX(), __BIGGEST_ALIGNMENT__);
+            float* norm_y_n = (float*)transformed_points.getNormalY();
+            float* norm_z_n = (float*)transformed_points.getNormalZ();
 
             // New labels
             uint8_t* labels_n = (uint8_t*)transformed_points.getLabels();
@@ -94,11 +94,11 @@ namespace egocylindrical
                     
                     pto.transform(x_p, y_p, z_p, x_n[p], y_n[p], z_n[p]);  
 
-                    // float nx_p = norm_x[p];
-                    // float ny_p = norm_y[p];
-                    // float nz_p = norm_z[p];
+                    float nx_p = norm_x[p];
+                    float ny_p = norm_y[p];
+                    float nz_p = norm_z[p];
 
-                    // pto.rotate(nx_p, ny_p, nz_p, norm_x_n[p], norm_y_n[p], norm_z_n[p]);         
+                    pto.rotate(nx_p, ny_p, nz_p, norm_x_n[p], norm_y_n[p], norm_z_n[p]);         
                                     
                     float range_squared= worldToRangeSquared(x_n[p],z_n[p]);
                 
