892fbaad223816081f06e5405bbc9ba2c7aedd50
Maxwell Asselmeier <asselmeier.max@gmail.com>
2025-07-27 16:56:13 -0400

    added egocylindrical propagator node

--- src/egocylindrical_propagator_node.cpp
+++ src/egocylindrical_propagator_node.cpp
@@ -14,15 +14,25 @@ namespace egocylindrical
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "egocylindrical_propagator");
-    
-    ros::NodeHandle nh;
-    ros::NodeHandle pnh("~");
-    
-    egocylindrical::EgoCylindricalPropagator s(nh, pnh);
+    // ros::init(argc, argv, "egocylindrical_propagator");
+    rclcpp::init(argc, argv);
+
+    rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared(
+        "egocylindrical_propagator",
+        rclcpp::NodeOptions()
+            .allow_undeclared_parameters(true)
+            .automatically_declare_parameters_from_overrides(true));
+
+    // ros::NodeHandle nh;
+    // ros::NodeHandle pnh("~");
+
+    egocylindrical::EgoCylindricalPropagator s(node);
     s.init();
     
-    ros::spin();
-    ros::MultiThreadedSpinner spinner;
-    spinner.spin();
+    rclcpp::spin(node);
+    // ros::MultiThreadedSpinner spinner;
+    rclcpp::executors::MultiThreadedExecutor executor;
+
+    executor.add_node(node);
+    executor.spin();
 }
