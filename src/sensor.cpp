5cd42e7c57770d3b3ffc83f06021906d6a66c213
Maxwell Asselmeier <mass@gatech.edu>
2025-12-09 14:57:42 -0500

    functional for three cameras

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -65,13 +65,17 @@ namespace egocylindrical
 
             //sensor_nh.getParam("publish_update", publish_update);
             //sensor_nh.getParam("raytrace", raytrace);
-            bool publish_update_res = get_param("sensors.semantic_depth.publish_update", publish_update);
-            bool raytrace_res = get_param("sensors.semantic_depth.raytrace", raytrace);
-            if (publish_update_res && raytrace_res) // get_name(name) && 
-            {
-                return true;
-            }
-            return false;
+            // bool publish_update_res = get_param("sensors.semantic_depth.publish_update", publish_update);
+            // bool raytrace_res = get_param("sensors.semantic_depth.raytrace", raytrace);
+            publish_update = true;
+            raytrace = true;
+
+            return true;
+            // if (publish_update_res && raytrace_res) // get_name(name) && 
+            // {
+            //     return true;
+            // }
+            // return false;
       }
 
   } //end namespace utils
--- src/sensor_collection.cpp
+++ src/sensor_collection.cpp
@@ -61,7 +61,7 @@ namespace egocylindrical
             // }
             // else 
 
-            if (name == "camera0") //  || name == "camera1"
+            if (name == "camera0" || name == "camera1" || name == "camera2")
             {
                 sensor = std::make_shared<SemanticDepthImageSensor>(node, name, buffer);
             }
@@ -148,7 +148,7 @@ namespace egocylindrical
             // };
             
             // std::vector<std::string> sensor_names = get_sensor_names3(); // sensor_root_nh
-            std::vector<std::string> sensor_names = { "camera0"}; // , "camera1" 
+            std::vector<std::string> sensor_names = { "camera0", "camera1", "camera2"}; // , "camera1" 
 
             for (const auto& name : sensor_names)
             {
